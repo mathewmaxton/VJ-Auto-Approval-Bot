7f4bcbc2754a271679ff56c745420558e777a441
mathewmaxton <152367165+mathewmaxton@users.noreply.github.com>
2024-01-19 21:56:02 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,7 +10,7 @@ class Config:
     API_HASH = getenv("API_HASH", "8d1de558964d482b32b9b65d0e2c5377")
     BOT_TOKEN = getenv("BOT_TOKEN", "6799250953:AAF3WsUC9gFfO_R6USYG42rxzzNCyw_r3nQ")
     FSUB = getenv("FSUB", "billabombay")
-    CHID = int(getenv("CHID", "--1001742846530"))
+    CHID = int(getenv("CHID", "-1001742846530"))
     SUDO = list(map(int, getenv("SUDO", "5777436923").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://mathewmaxton:4PLBCZ4xhxlYB7II@cluster0.udnd6vl.mongodb.net/?retryWrites=true&w=majority")
     
